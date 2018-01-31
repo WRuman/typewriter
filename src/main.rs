a64f471e8e4822c1b827e444a0f95f482e2adbb0
Will Ruman <WRuman@users.noreply.github.com>
2018-01-30 19:04:33 -0600

    Take positional args (#2)
    * Takes command line argument as message to type, otherwise default
    
    * Takes first argument of many, no matter how many

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 extern crate typewriter;
 
 use typewriter::type_print;
+use std::env;
+
+static MSG: &str = "Computers are like any other machine. They're either a benefit or a hazard. If they're a benefit, it's not my problem";
 
 fn main() {
-    let msg = "Computers are like any other machine. They're either a benefit or a hazard. If they're a benefit, it's not my problem";
-    type_print(msg, 90);
+    let args: Vec<String> = env::args().collect();
+    let output = match args.len() {
+        n if n >= 2 => args[1].as_str(),
+        _ => MSG
+    };
+    type_print(output, 90);
 }
